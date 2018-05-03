680ea3ba21091c4f728cdeb0abc2d8812a42f1de
Joannes Paanakker <j.paanakker@upcmail.nl>
2018-05-03 16:53:04 +0200

    first push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,41 @@
 class PagesController < ApplicationController
   def home
+    @language = "ENG"
+    @page = "home"
+  end
+
+  def team
+    @language = "ENG"
+    @page = "team"
+  end
+
+  def products
+    @language = "ENG"
+    @page = "products"
+  end
+
+  def contact
+    @language = "ENG"
+    @page = "contact"
+  end
+
+  def nl
+    @language = "NL"
+    @page = "nl"
+  end
+
+  def nlteam
+    @language = "NL"
+    @page = "nlteam"
+  end
+
+  def producten
+    @language = "NL"
+    @page = "producten"
+  end
+
+  def nlcontact
+     @language = "NL"
+     @page = "nlcontact"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'team', to: 'pages#team', as: :team
+  get 'products', to: 'pages#products', as: :products
+  get 'contact', to: 'pages#contact', as: :contact
+
+  get 'nl', to: 'pages#nl', as: :nl
+  get 'nlteam', to: 'pages#nlteam', as: :nlteam
+  get 'producten', to: 'pages#producten', as: :producten
+  get 'nlcontact', to: 'pages#nlcontact', as: :nlcontact
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
